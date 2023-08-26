514dfb29b4cc44294d2d284d5be4fd683e5c7104
shabarinath4 <shabarinath.s.nair@gmail.com>
2023-08-26 07:22:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="🦙💬 Robotics Teachbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
+    st.title('🦙💬 Robotics Teach Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
